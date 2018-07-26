bd5355afea35011804bcff55e6d615c8a393aad7
Your Name <you@example.com>
2018-07-26 16:56:34 +0400

    uncommented server

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ app.use(express.static("."));
 app.get('/', function (req, res) {
    res.redirect('index.html');
 });
-/*var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require('http').Server(app);
+/*var io = require('socket.io')(server);
 
 
 server.listen(3000);*/
