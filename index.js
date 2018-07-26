6019ffd398cdd171ba543b56fbe2dd8e5ca88a10
Your Name <you@example.com>
2018-07-26 17:25:40 +0400

    public

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 // Define the port to run on
 app.set('port', process.env.PORT || 3000);
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 
 var server = app.listen(app.get('port'), function() {
