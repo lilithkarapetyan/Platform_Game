b3043a25fc1674448c5bf146b3822afc255c43bd
Your Name <you@example.com>
2018-08-02 13:20:44 +0400

    only alert

--- script/class.js
+++ script/class.js
@@ -248,8 +248,8 @@ class Player extends Parent {
     win() {
         this.won = true;
         alert("You won, please save your level");
-        gameStarted = false;
-        construct(data)
+        //gameStarted = false;
+        //construct(data)
         /*
         https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal2
         */
