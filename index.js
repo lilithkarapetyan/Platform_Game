a9146f13a9e363e445c28625568d9e11097125a1
lilithkarapetyan <lilith.karapetyann@gmail.com>
2018-08-21 20:33:03 +0400

    trying to fix the server

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-/*
-var express = require('express');
+/*var express = require('express');
 var path = require('path');
 var app = express();
 
@@ -15,7 +14,6 @@ var server = app.listen(app.get('port'), function() {
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-
 app.use(express.static("."));
 app.get('/', function (req, res) {
    res.redirect('index.html');
